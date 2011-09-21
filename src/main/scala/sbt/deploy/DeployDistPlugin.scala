94cb24e8ec48cc3b33e0ecbb9ba54dc82b2dc424
Karim Osman <karim.osman@reaktor.fi>
2011-09-21 19:32:17 +0300

    sbt-deploy: Create symbolic links to the latest release
    Signed-off-by: Karim Osman <karim.osman@reaktor.fi>

--- src/main/scala/sbt/deploy/DeployDistPlugin.scala
+++ src/main/scala/sbt/deploy/DeployDistPlugin.scala
@@ -7,18 +7,38 @@ import SecureConnectivityPlugin._
 
 object DeployDistPlugin extends Plugin {
   val deployDist = TaskKey[Unit]("deploy-dist", "Deploy distribution package")
+  val instPkgPath = TaskKey[File]("inst-pkg-path")
   val instDirParent = SettingKey[File]("inst-dir-parent")
   val instDir = TaskKey[File]("inst-dir")
+  val latestInstDir = TaskKey[File]("latest-inst-dir")
+  val instJarPath = TaskKey[File]("inst-jar-path")
+  val latestInstJarPath = TaskKey[File]("latest-inst-jar-path")
   lazy val deployDistSettings = Seq(
-    instDir <<= (instDirParent, name) map { (parent, name) =>
-      new File(parent, name + "-latest")
+    instDir <<= (instDirParent, name, version, scalaVersion) map { (parent, name, version, scalaVersion) =>
+      new File(parent, "%s_%s-%s".format(name, scalaVersion, version))
     },
-    deployDist <<= (streams, identityFile, user, host, packageDist, instDir) map { (out, idFile, user, host, pkgPath, instDir) =>
+    instPkgPath <<= (instDir, packageDist) map { (instDir, pkgPath) =>
+      new File(instDir, pkgPath.getName)
+    },
+    latestInstDir <<= (instDir, name) map { (instDir, name) =>
+      new File(instDir.getParent, name)
+    },
+    instJarPath <<= (instDir) map { (instDir) =>
+      new File(instDir, instDir.getName + ".jar")
+    },
+    latestInstJarPath <<= (latestInstDir, name) map { (latestInstDir, name) =>
+      new File(latestInstDir, name + ".jar")
+    },
+    deployDist <<= (streams, identityFile, user, host, packageDist, instDir, latestInstDir, instJarPath, latestInstJarPath) map { 
+        (out, idFile, user, host, pkgPath, instDir, latestInstDir, instJarPath, latestInstJarPath) =>
       scp(out.log, idFile, user, host, pkgPath, instDir.getParent)
       ssh(out.log, idFile, user, host,
         "cd " + instDir.getParent,
-        "rm -rf " + instDir.getName,
-        "unzip -q -d " + instDir.getName + " " + pkgPath.getName)
+        "rm -rf " + instDir.getName + " " + latestInstDir,
+        "unzip -q -d " + instDir.getName + " " + pkgPath.getName,
+        "ln -s " + instJarPath.getParent + " " + latestInstJarPath.getParent,
+        "ln -s " + instJarPath + " " + latestInstJarPath
+      )
     }
   )
 }
